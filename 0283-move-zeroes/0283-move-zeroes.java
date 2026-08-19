1a0508bff2c6dd4541822c2fe4f5b686d8b8acc5
Kartik Sharma <156449249+officialxkarry@users.noreply.github.com>
2026-08-19 12:25:27 +0530

    Time: 44 ms (5.71%), Space: 47.9 MB (38.74%) - LeetHub

--- 0283-move-zeroes/0283-move-zeroes.java
+++ 0283-move-zeroes/0283-move-zeroes.java
@@ -1,20 +1,43 @@
 class Solution {
-    public int[] moveZeroes(int[] nums){
-        for(int i=0;i<nums.length;i++){
-            if(nums[i]==0)
+    public int[] moveZeroes(int[] nums)
+    {
+        return moveZero(nums,0);
+        // for(int i=0;i<nums.length;i++){
+        //     if(nums[i]==0)
+        //     {
+        //         for(int j=i+1;j<nums.length;j++)
+        //         {
+        //             if(nums[j]!=0)
+        //             {
+        //                 int temp=nums[i];
+        //                 nums[i]=nums[j];
+        //                 nums[j]=temp;
+        //                 break;
+        //             }
+        //         }   
+        //     }
+        // }
+        // return nums;
+    }
+    public static int[] moveZero(int[] nums,int idx)
+    {
+        if(idx==nums.length)
+        {
+            return nums;
+        }
+        if(nums[idx]==0)
+        {
+            for(int j=idx+1;j<nums.length;j++)
             {
-                for(int j=i+1;j<nums.length;j++)
+                if(nums[j]!=0)
                 {
-                    if(nums[j]!=0)
-                    {
-                        int temp=nums[i];
-                        nums[i]=nums[j];
-                        nums[j]=temp;
-                        break;
-                    }
-                }   
+                    nums[idx]=nums[idx]^nums[j];
+                    nums[j]=nums[idx]^nums[j];
+                    nums[idx]=nums[idx]^nums[j];
+                    break;
+                }
             }
         }
-        return nums;
+    return moveZero(nums,idx+1);
     }
 }
