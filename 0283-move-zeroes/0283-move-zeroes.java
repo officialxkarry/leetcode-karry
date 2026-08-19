d71cfa7f3d79082aa5dc25a80fd7dc689f3f4659
Kartik Sharma <156449249+officialxkarry@users.noreply.github.com>
2026-08-19 12:41:27 +0530

    Time: 2 ms (92.04%), Space: 47.4 MB (93.77%) - LeetHub

--- 0283-move-zeroes/0283-move-zeroes.java
+++ 0283-move-zeroes/0283-move-zeroes.java
@@ -1,7 +1,23 @@
 class Solution {
     public int[] moveZeroes(int[] nums)
     {
-        return moveZero(nums,0);
+        int lastNonZeroFoundAt=0;
+
+        // Move all non-zero elements forward
+        for (int i = 0; i < nums.length; i++) {
+            if (nums[i] != 0) {
+                int temp = nums[i];
+                nums[i] = nums[lastNonZeroFoundAt];
+                nums[lastNonZeroFoundAt]=temp;
+                lastNonZeroFoundAt++;
+            }
+        }
+
+        return nums;
+    
+
+// Recusion call -------> return moveZero(nums,0);
+//Using 2 For Loops;
         // for(int i=0;i<nums.length;i++){
         //     if(nums[i]==0)
         //     {
@@ -19,25 +35,26 @@ class Solution {
         // }
         // return nums;
     }
-    public static int[] moveZero(int[] nums,int idx)
-    {
-        if(idx==nums.length)
-        {
-            return nums;
-        }
-        if(nums[idx]==0)
-        {
-            for(int j=idx+1;j<nums.length;j++)
-            {
-                if(nums[j]!=0)
-                {
-                    nums[idx]=nums[idx]^nums[j];
-                    nums[j]=nums[idx]^nums[j];
-                    nums[idx]=nums[idx]^nums[j];
-                    break;
-                }
-            }
-        }
-    return moveZero(nums,idx+1);
-    }
+//Using Recursion;
+    // public static int[] moveZero(int[] nums,int idx)
+    // {
+    //     if(idx==nums.length)
+    //     {
+    //         return nums;
+    //     }
+    //     if(nums[idx]==0)
+    //     {
+    //         for(int j=idx+1;j<nums.length;j++)
+    //         {
+    //             if(nums[j]!=0)
+    //             {
+    //                 nums[idx]=nums[idx]^nums[j];
+    //                 nums[j]=nums[idx]^nums[j];
+    //                 nums[idx]=nums[idx]^nums[j];
+    //                 break;
+    //             }
+    //         }
+    //     }
+    //     return moveZero(nums,idx+1);
+    // }
 }
